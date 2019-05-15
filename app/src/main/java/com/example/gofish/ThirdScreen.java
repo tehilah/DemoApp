53416fdc6c431c3d2b9ee54388bf024d17d53d5a
Tehilah <tehilah.nagar@mail.huji.ac.il>
2019-05-15 13:54:24 +0300

    minor changes

--- app/src/main/java/com/example/gofish/ThirdScreen.java
+++ app/src/main/java/com/example/gofish/ThirdScreen.java
@@ -5,9 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 
 public class ThirdScreen extends AppCompatActivity {
